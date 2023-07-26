aaadd82ca03f5ed1d90d4aaf14e6ed5156ea400b
Viennarz Curtiz <viennarzc@gmail.com>
2023-07-26 22:43:32 +0800

    States

--- FilipinoDriversManualReviewer/FilipinoDriversManualReviewer/QuestionAnswerView.swift
+++ FilipinoDriversManualReviewer/FilipinoDriversManualReviewer/QuestionAnswerView.swift
@@ -8,35 +8,101 @@
 import SwiftUI
 
 struct QuestionAnswerView: View {
+    
+    let question: String
+    let answerOptions: [String]
+    let correctAnswer: String
+    
+    @State private var selectedOption: String?
+    @State private var isCorrectAnswer: Bool = false
+    @State private var hasCommittedAnswer: Bool = false
+    
+    func setOptionColor(selectedAnswer: String?, equalTo current: String) -> Color {
+        guard let selectedAnswer = selectedAnswer else {
+            return .gray
+        }
+        
+        if !hasCommittedAnswer, !isCorrectAnswer, selectedAnswer == current {
+            return .black
+        }
+        
+        if hasCommittedAnswer, !isCorrectAnswer, selectedAnswer == current {
+            return .red
+        }
+        
+        if isCorrectAnswer, selectedAnswer == current {
+            return .green
+        }
+        
+        return .gray
+    }
+    
     var body: some View {
         VStack {
-            Text("What is the fastest way to drive?")
+            Text(question)
                 .font(.title2)
             
             Spacer(minLength: 32)
             
-            VStack {
-                
+            VStack(spacing: 16) {
+                ForEach(answerOptions, id: \.self) { item in
+                    VStack {
+                        Text(item)
+                            .fontWeight(selectedOption == item ? .medium : .regular)
+                            .foregroundColor(setOptionColor(selectedAnswer: selectedOption, equalTo: item))
+                    }
+                    .padding()
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .background(RoundedRectangle(
+                        cornerRadius: 8).stroke(setOptionColor(selectedAnswer: selectedOption, equalTo: item), lineWidth: selectedOption == item ? 3 : 1))
+                    .contentShape(Rectangle())
+                    .onTapGesture {
+                        self.selectedOption = item
+                    }
+                }
+                .disabled(isCorrectAnswer)
+                .onChange(of: selectedOption) { newValue in
+                    hasCommittedAnswer = false
+                }
             }
+            .padding(.horizontal)
             
-            Button(action: {
-                
-            }, label: {
-                Text("Check Answer")
+            Spacer(minLength: 64)
+            
+            Button(action: checkAnswer, label: {
+                Text(isCorrectAnswer ? "Continue" : "Check Answer")
                     .frame(maxWidth: .infinity, alignment: .center)
                     .padding()
                     .font(.body.bold())
                 
+                
             })
             .buttonStyle(.borderedProminent)
             .padding(.horizontal)
+            .tint(.black)
+            .disabled(selectedOption == nil)
             
+        }
+    }
+    
+    private func checkAnswer() {
+        hasCommittedAnswer = true
+        
+        if correctAnswer == selectedOption {
+            isCorrectAnswer = true
+        } else {
+            isCorrectAnswer = false
         }
     }
 }
 
 struct QuestionAnswerView_Previews: PreviewProvider {
     static var previews: some View {
-        QuestionAnswerView()
+        QuestionAnswerView(question: "What is the fastest way to drive?", answerOptions: [
+            "Sleep",
+            "Take a seat in the passengers seat and look around",
+            "Never forget to turn on engine",
+            "Wear seatbelt"
+        ], correctAnswer: "Wear seatbelt")
     }
 }
